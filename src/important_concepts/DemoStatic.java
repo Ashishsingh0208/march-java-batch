856310c22c2d2ecaec88bd02d4ac04d04372bdbd
60manojkumar@gmail.com <Motivated@2019>
2022-04-08 07:40:50 +0530

    static and final keywords

--- src/important_concepts/DemoStatic.java
+++ src/important_concepts/DemoStatic.java
@@ -3,10 +3,17 @@ package important_concepts;
 public  class DemoStatic {
 
     static int age=23;
+    String name="JAVA";
 
     public static void hello(){
-
+      // System.out.println(name);// WE CAN NOT USE NON STATIC DATA AS IT IS PART OF OBJECT
        System.out.println("This is STATIC HELLO Method...");
+    }
+
+    // overloading of static method
+    public static void hello(int age){
+        // System.out.println(name);// WE CAN NOT USE NON STATIC DATA AS IT IS PART OF OBJECT
+        System.out.println("This is STATIC HELLO Method...");
     }
 
 
